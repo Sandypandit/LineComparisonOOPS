f2f7dc5b529bf413194353045b3bccbd8340125c
Sandypandit <sandypandit86@gmail.com>
2021-10-19 23:26:32 +0530

    checking Equality of two lines using OOPS concept

--- LineComparison.java
+++ LineComparison.java
@@ -7,11 +7,25 @@ public class LineComparison {
 	
 	public double Length() {
 	    //used decimal format to limit numbers after decimal
-		DecimalFormat df = new DecimalFormat("#.00");	  
+		DecimalFormat df = new DecimalFormat("#.00");
+		
 		double length = Math.sqrt(Math.pow((this.x2-this.x1),2)+Math.pow((this.y2-this.y1),2));
 		System.out.println("Length of the Line is "+df.format(length)+"\n");
+		
+		//converting to object so that equals method can use it
 		Double linelength = Double.valueOf(length); 
 		return linelength;
+	}
+
+	static void Equal(Double lineOne, Double lineTwo) {
+		//creating method to check equality of any two lines
+		if(lineOne.equals(lineTwo)){ 
+			System.out.println("\ngiven two lines are equal");
+			}
+		else{ 
+			System.out.println("\nGiven two lines are not equal"); 
+			}
+		
 	}
 	
 	public LineComparison() {
@@ -33,6 +47,12 @@ public class LineComparison {
 				
 		LineComparison line1 = new LineComparison();
 		line1.Length();
+		
+		LineComparison line2 = new LineComparison();
+		line2.Length();
+		
+		//passing line 1 and line 2 lengths in Equals method
+		Equal(line1.Length(),line2.Length());
 
 		
 	}
