cc855d85cee882f5c0ed9230a3171c0ed0916685
Sandypandit <sandypandit86@gmail.com>
2021-10-19 23:06:42 +0530

    Line Comparison program master branch

--- LineComparison.java
+++ LineComparison.java
@@ -1,32 +1,40 @@
 package com.bridgelabz;
-import java.util.Random;
+import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class LineComparison {
-	int x1, x2, y1, y2;
+	int x1=0, x2=0, y1=0, y2=0;
 	
-	public void Length() {
+	public double Length() {
 	  //used decimal format to limit numbers after decimal
 	  DecimalFormat df = new DecimalFormat("#.00");	  
 	  double length = Math.sqrt(Math.pow((this.x2-this.x1),2)+Math.pow((this.y2-this.y1),2));
-	  System.out.println("Length of the Line is "+df.format(length));
+	  System.out.println("Length of the Line is "+df.format(length)+"\n");
+	  Double linelength = Double.valueOf(length); 
+	  return linelength;
 	}
 	
-	public LineComparison(int x1,int x2,int y1,int y2) {
-		this.x1=x1; //generating constructor
+	public LineComparison() {
+		int lineNo = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("Enter points of line %d in Following Sequence"
+				+ "\nX1\nX2\nY1\nY2\n",lineNo++);
+		int x1=sc.nextInt(); 
+		int x2=sc.nextInt(); //getting end points of lines
+		int y1=sc.nextInt(); 
+		int y2=sc.nextInt();
+		
+		this.x1=x1;
 		this.x2=x2;
 		this.y1=y1;
 		this.y2=y2;
 	}
 	public static void main(String[] args) {
-		Random rd = new Random();
-		int x1=rd.nextInt(10); int x2=rd.nextInt(10); //getting end points of lines
-		int y1=rd.nextInt(10); int y2=rd.nextInt(10);
-		LineComparison line = new LineComparison(x1,x2,y1,y2);
-		
-		line.Length();
+				
+		LineComparison line1 = new LineComparison();
+		line1.Length();
+
 		
 	}
 
 }
-
